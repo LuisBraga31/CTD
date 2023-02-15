d745140800ad2eecb7d1ed7eb85052682f3974f3
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-02-15 20:01:19 -0300

    Corrigindo erros

--- Atividade 14-02/script.js
+++ Atividade 14-02/script.js
@@ -1,65 +1,49 @@
-// 01 - Confirmação de jogo e inserir nome
 
-let confirmacao = confirm('Você deseja jogar?');
-
-    if(confirmacao === true) {
-
-        let nome = prompt('Digite seu nome para Jogar: ');
-        comecarJogo(nome);
-
-    } else {
-
-    }
-
-// 02 - Função para começar jogo
-
-    function comecarJogo(nome) {
+function start(nome) {
     
-        let pontuacaoJogador = 0;
-        let pontuacaoMaquina = 0;
-        let verificador = 0;
+    let pontuacaoJogador = 0;
+    let pontuacaoMaquina = 0;
+    let verificador = 0;
         
-        do {
+    do {
             
-            // 01 - Definindo variáveis de jogada
-            let jogadaUsuario = parseInt(prompt('Vamos Jogar! Tesoura: 1, Papel: 2, Pedra: 3'));
+        // 01 - Definindo variáveis de jogada
+            let jogadaUsuario = parseInt(prompt('Vamos Jogar! Pedra: 1, Papel: 2, Tesoura: 3'));
             let jogadaMaquina = Math.floor(Math.random() * 3) + 1;
 
-            // 02 - Decisão de jogo
+        // 02 - Decisão de jogo
 
             if (jogadaUsuario === 1 && jogadaMaquina === 1) {
-                alert(`Máquina: Pedra \n${nome}: Pedra. \nO jogo deu Velha!`);
+                alert(`${nome}: Pedra \nMáquina: Pedra. \nO jogo deu Velha!`);
             }
             else if (jogadaUsuario === 2 && jogadaMaquina === 2) {
-                alert(`Máquina: Papel \n${nome}: Papel. \nA máquina venceu! `);
-                pontuacaoMaquina++;
+                alert(`${nome}: Papel \nMáquina: Papel. \nO jogo deu Velha!`);
             }
             else if (jogadaUsuario === 3 && jogadaMaquina === 3) {
-                alert(`Máquina: Pedra \n${nome}: Tesoura. \nA máquina venceu! `);
-                pontuacaoMaquina++;
+                alert(`${nome}: Pedra \nMáquina: Tesoura. \nO jogo deu Velha! `);
             }
             else if (jogadaUsuario === 1 && jogadaMaquina === 2) {
-                alert(`Máquina: Pedra \n${nome}: Papel. \nA máquina venceu! `);
+                alert(`${nome}: Pedra \nMáquina: Papel. \nA máquina venceu! `);
                 pontuacaoMaquina++;
             }
             else if (jogadaUsuario === 2 && jogadaMaquina === 3) {
-                alert(`Máquina: Papel \n${nome}: Tesoura. \nA máquina venceu!`);
+                alert(`${nome}: Papel \nMáquina: Tesoura. \nA máquina venceu!`);
                 pontuacaoMaquina++;
             }
             else if (jogadaUsuario === 3 && jogadaMaquina === 1) {
-                alert(`Máquina: Tesoura \n${nome}: Pedra. \nA máquina venceu!`);
+                alert(`${nome}: Tesoura \nMáquina: Pedra. \nA máquina venceu!`);
                 pontuacaoMaquina++;
             }
             else if (jogadaUsuario === 1 && jogadaMaquina === 3) {
-                alert(`Máquina: Pedra \n${nome}: Tesoura. \nO usuário ${nome} venceu!`);
+                alert(`${nome}: Pedra \nMáquina: Tesoura. \nO usuário ${nome} venceu!`);
                 pontuacaoJogador++;
             }
             else if (jogadaUsuario === 2 && jogadaMaquina === 1) {
-                alert(`Máquina: Papel \n${nome}: Pedra. \nO usuário ${nome} venceu!`);
+                alert(`${nome}: Papel \nMáquina: Pedra. \nO usuário ${nome} venceu!`);
                 pontuacaoJogador++;
             } 
             else if (jogadaUsuario === 3 && jogadaMaquina === 2) {
-                alert(`Máquina: Tesoura \n${nome}: Papel. \nO usuário ${nome} venceu!`);
+                alert(`${nome}: Tesoura \nMáquina: Papel. \nO usuário ${nome} venceu!`);
                 pontuacaoJogador++;
             } 
             else {
@@ -67,7 +51,6 @@ let confirmacao = confirm('Você deseja jogar?');
             }
 
             // 03 - Verificador de Partida
-
             if (pontuacaoJogador === 2) {
                 verificador = 1;
                 alert(`O usuário ${nome} atingiu 2 pontos e venceu !!!`);
@@ -78,6 +61,33 @@ let confirmacao = confirm('Você deseja jogar?');
             } else {
             }
 
-        } while (verificador === 0); 
+    } while (verificador === 0); 
 
+}
+
+function confirmacao() {
+    
+    var confirmacao = confirm('Você deseja jogar?');
+    
+    if (confirmacao === true) {
+
+        let nome = prompt('Digite seu nome para Jogar: ');
+        start(nome);
+    
+        } else {
+    
+        alert('Obrigado pela visita!');
+    
     }
+}
+
+confirmacao();
+
+
+// DIFICULDADE
+
+// FACIL
+
+//NORMAL
+
+// DIFICIL
