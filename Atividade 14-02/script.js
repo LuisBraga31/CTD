db9941f57ab54027ec04ed91adcc3cdf0808f682
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-02-23 19:00:46 -0300

    Adicionando dificuldade ao jogo de Jokenpo

--- Atividade 14-02/script.js
+++ Atividade 14-02/script.js
@@ -1,15 +1,51 @@
 
-function start(nome) {
+function start(nome, dificuldade) {
     
     let pontuacaoJogador = 0;
     let pontuacaoMaquina = 0;
     let verificador = 0;
-        
+
     do {
-            
+        var jogadaUsuario;
+        var jogadaMaquina;  
+        
         // 01 - Definindo variáveis de jogada
-            let jogadaUsuario = parseInt(prompt('Vamos Jogar! Pedra: 1, Papel: 2, Tesoura: 3'));
-            let jogadaMaquina = Math.floor(Math.random() * 3) + 1;
+        if (dificuldade == 1) {
+            
+            jogadaUsuario = parseInt(prompt('Vamos Jogar! Pedra: 1, Papel: 2, Tesoura: 3'));
+            var easy = Math.floor(Math.random() * 3) + 1;
+
+            if ((easy === 1 || easy === 3) && jogadaUsuario === 1) {
+                jogadaMaquina = 3;
+            } else if ((easy === 1 || easy === 3) && jogadaUsuario === 2) {
+                jogadaMaquina = 1;
+            } else if ((easy === 1 || easy === 3) && jogadaUsuario === 3) {
+                jogadaMaquina = 2;
+            } else {
+                jogadaMaquina = Math.floor(Math.random() * 3) + 1; 
+            }
+        } 
+        
+        else if (dificuldade == 2) {
+            jogadaUsuario = parseInt(prompt('Vamos Jogar! Pedra: 1, Papel: 2, Tesoura: 3'));
+            jogadaMaquina = Math.floor(Math.random() * 3) + 1;
+        } 
+        
+        else if (dificuldade == 3) {
+            jogadaUsuario = parseInt(prompt('Vamos Jogar! Pedra: 1, Papel: 2, Tesoura: 3'));
+            var hard = Math.floor(Math.random() * 3) + 1;
+
+            if ((hard === 1 || hard === 3) && jogadaUsuario === 1) {
+                jogadaMaquina = 2;
+            } else if ((hard === 1 || hard === 3) && jogadaUsuario === 2) {
+                jogadaMaquina = 3;
+            } else if ((hard === 1 || hard === 3) && jogadaUsuario === 3) {
+                jogadaMaquina = 1;
+            } else {
+                jogadaMaquina = Math.floor(Math.random() * 3) + 1; 
+            }
+        };
+
 
         // 02 - Decisão de jogo
 
@@ -20,7 +56,7 @@ function start(nome) {
                 alert(`${nome}: Papel \nMáquina: Papel. \nO jogo deu Velha!`);
             }
             else if (jogadaUsuario === 3 && jogadaMaquina === 3) {
-                alert(`${nome}: Pedra \nMáquina: Tesoura. \nO jogo deu Velha! `);
+                alert(`${nome}: Tesoura \nMáquina: Tesoura. \nO jogo deu Velha! `);
             }
             else if (jogadaUsuario === 1 && jogadaMaquina === 2) {
                 alert(`${nome}: Pedra \nMáquina: Papel. \nA máquina venceu! `);
@@ -46,7 +82,11 @@ function start(nome) {
                 alert(`${nome}: Tesoura \nMáquina: Papel. \nO usuário ${nome} venceu!`);
                 pontuacaoJogador++;
             } 
-            else {
+            else if(isNaN(jogadaUsuario)){
+                verificador = 1;
+                alert('Fim de jogo!');
+            } else {
+                console.log(jogadaUsuario);
                 alert('Essa opção não existe !!!');
             }
 
@@ -59,9 +99,27 @@ function start(nome) {
                 verificador = 1;
                 alert('A máquina atingiu 2 pontos e venceu !!!');
             } else {
+            
             }
 
     } while (verificador === 0); 
+
+}
+
+function dificuldade() {
+    let verificadorDificuldade = 0;
+    do {
+        
+        let dificuldade = prompt('Dificuldade: Fácil - 1, Normal -2, Difícil -3 ');
+        console.log(dificuldade > 3 && dificuldade < 1);
+        if(dificuldade > 3 || dificuldade < 1) {
+            alert('Essa dificuldade não existe');
+        } else {
+            verificador = 1;
+            return dificuldade;
+        }
+        console.log(dificuldade);
+    } while (verificadorDificuldade === 0); 
 
 }
 
@@ -72,7 +130,11 @@ function confirmacao() {
     if (confirmacao === true) {
 
         let nome = prompt('Digite seu nome para Jogar: ');
-        start(nome);
+
+        let teste = dificuldade();
+        console.log(teste);
+        alert(`Vamos começar o jogo ${Luis}, o nível de dificu`)
+        start(nome, teste);
     
         } else {
     
@@ -82,6 +144,7 @@ function confirmacao() {
 }
 
 confirmacao();
+
 
 
 // DIFICULDADE
